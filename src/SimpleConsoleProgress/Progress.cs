71a45ad3dcad719e569ca3ddfa68dca2bda91b84
Filip Liwiński <fili@netcompany.com>
2023-10-20 00:00:30 +0200

    Applied editorConfig settings

--- src/SimpleConsoleProgress/Progress.cs
+++ src/SimpleConsoleProgress/Progress.cs
@@ -1,6 +1,4 @@
-// MIT License
-//  
-//  Copyright (c) 2020-2023 Filip Liwiński
+// Copyright (c) Filip Liwiński
 //  
 //  Licensed under the MIT License. See the LICENSE file in the project root for license information.
 //
